de3db3d6320f92ed064c5e985437c714dfc6707b
shiponirfan <shiponirfan.dev@gmail.com>
2024-08-13 12:30:28 +0600

    Greeting App Completed

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -5,6 +5,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Greeting App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Hello, World!',
+              style: TextStyle(
+                  color: Colors.red, fontWeight: FontWeight.w700, fontSize: 24),
+            ),
+            const Text(
+              'Welcome to Flutter!',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+            Image.asset('assets/images/flutter_logo.png'),
+            ElevatedButton(
+                onPressed: () {
+                  const snackBar = SnackBar(
+                    content: Text('Button Pressed!'),
+                    backgroundColor: Colors.green,
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 15)),
+                child: const Text('Press Me'))
+          ],
+        ),
+      ),
+    );
   }
 }
